ba4c5ddc77f77f71c5bc2b97d86d3eb83d4d62ec
Eduardo Ortiz <eduardo_ortiz09@hotmail.com>
2022-11-29 01:04:58 -0600

    Fix errrors in server

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dal = require('./dal');
+const path = require('path')
+
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 const admin = require('./admin');
 
@@ -11,6 +15,10 @@ const port = 4000;
 
 app.use(express.static('public/build'));
 app.use(cors());
+
+
+// Swagger
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // create user account
 app.post('/account/create/:name/:email/:password', function(req, res){
@@ -22,72 +30,90 @@ app.post('/account/create/:name/:email/:password', function(req, res){
 
 // All accounts
 app.get('/account/all', (req, res) => {
-  const idToken = req.headers.authorization;
-  admin.auth().verifyIdToken(idToken)
-    .then(function() {
-      dal.all()
-        .then(docs => {
-          res.send(docs);
-        });
-    }).catch(function(error){
-      console.log('error: ', error)
-      res.send('Authentication Fail!')
-    })
+  try {
+    const idToken = req.headers.authorization;
+    admin.auth().verifyIdToken(idToken)
+      .then(function() {
+        dal.all()
+          .then(docs => {
+            res.send(docs);
+          });
+      }).catch(function(error){
+        console.log('error: ', error)
+        res.send('Authentication Fail!')
+      })
+  } catch (e) {
+    res.send('Token error: ' + e.message )
+  }
 });
 
 // Get one account
 
 app.get('/account/:email', (req, res) => {
-  const idToken = req.headers.authorization;
-  admin.auth().verifyIdToken(idToken)
-    .then(function() {
-      let email = req.params.email
-      dal.searchUser(email)
-        .then(docs => {
-          res.send(docs);
-        });
-    }).catch(function(error){
-      console.log('error: ', error)
-      res.send('Authentication Fail!')
-    })
+  try {
+    const idToken = req.headers.authorization;
+    admin.auth().verifyIdToken(idToken)
+      .then(function() {
+        let email = req.params.email
+        dal.searchUser(email)
+          .then(docs => {
+            res.send(docs);
+          });
+      }).catch(function(error){
+        console.log('error: ', error)
+        res.send('Authentication Fail!')
+      })
+
+  } catch (e) {
+    res.send('Token error: ' + e.message )
+  }
 })
 
 // Update Balance
 
 app.put('/account/update/:email/:balance/:type', (req, res) => {
-  const idToken = req.headers.authorization;
-  admin.auth().verifyIdToken(idToken)
-    .then(function() {
-      let { email, balance, type } = req.params
-      dal.updateBalance(email, balance, type)
-        .then(docs => {
-          res.send(docs);
-        });
-    }).catch(function(error){
-      console.log('error: ', error)
-      res.send('Authentication Fail!')
-    })
+  try {
+    const idToken = req.headers.authorization;
+    admin.auth().verifyIdToken(idToken)
+      .then(function() {
+        let { email, balance, type } = req.params
+        dal.updateBalance(email, balance, type)
+          .then(docs => {
+            res.send(docs);
+          });
+      }).catch(function(error){
+        console.log('error: ', error)
+        res.send('Authentication Fail!')
+      })
+  } catch (e) {
+    res.send('Token error: ' + e.message )
+  }
 })
 
 // Get Transactions of user
 
 app.get('/transactions/:email', (req, res) => {
-  const idToken = req.headers.authorization;
-  admin.auth().verifyIdToken(idToken)
-    .then(function() {
-      let email = req.params.email
-      dal.getTransactions(email)
-        .then(docs => {
-          res.send(docs);
-        });
-    }).catch(function(error){
-      console.log('error: ', error)
-      res.send('Authentication Fail!')
-    })
+  try {
+    const idToken = req.headers.authorization;
+    admin.auth().verifyIdToken(idToken)
+      .then(function() {
+        let email = req.params.email
+        dal.getTransactions(email)
+          .then(docs => {
+            res.send(docs);
+          });
+      }).catch(function(error){
+        console.log('error: ', error)
+        res.send('Authentication Fail!')
+      })
+  } catch (e) {
+    res.send('Token error: ' + e.message )
+  }
 })
 
 app.delete('/account/delete/:email', (req, res) => {
-  const idToken = req.headers.authorization;
+  try {
+      const idToken = req.headers.authorization;
   admin.auth().verifyIdToken(idToken)
     .then(function() {
       let email = req.params.email
@@ -99,6 +125,14 @@ app.delete('/account/delete/:email', (req, res) => {
       console.log('error: ', error)
       res.send('Authentication Fail!')
     })
+
+  } catch (e) {
+    res.send('Token error: ' + e.message )
+  }
+})
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname+'/public/build/index.html'));
 })
 
 app.listen(port, () => {
